0500aca7a11bd91af45eb31c10700aeac4ab52fa
Александр Данильченко <mlemix32@gmail.com>
2022-04-21 00:08:59 +0300

    Добавьте файлы проекта.

---
+++ distance.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	double km;
+
+	cout << "Enter distance : ";
+	cin >> km;
+
+	double statMil = km / 0.62;
+	double nautMil = km / 0.54;
+
+	cout << "\n\tkm in statute miles : " << statMil << "\n\n";
+	cout << "\tkm in nautical miles : " << nautMil << "\n";
+
+	return 0;
+}
